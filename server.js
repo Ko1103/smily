b1839a8b1bad81fab18203482f883ac5d9f66845
atook <atok.reverse.1040@gmail.com>
2017-02-04 18:30:37 +0900

    SSL証明書の発行

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-var http = require('http');
-var fs = require('fs');
 
-var server = http.createServer('');
-server.on('request', function (req, res) {
-    fs.readFile(__dirname + '/public/index.html', 'utf-8', function (err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.write('not found!');
-            return res.end();
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        res.end();
-    });
-});
+// curl -k https://localhost:8000/
+const https = require('https');
+const fs = require('fs');
 
-server.listen(8080);
-console.log('listening on port 8080...');
+const options = {
+  key: fs.readFileSync('keys/ryans-key.pem'),
+  cert: fs.readFileSync('keys/ryans-cert.pem')
+};
+
+https.createServer(options, (req, res) => {
+      fs.readFile(__dirname + '/public/index.html', 'utf-8', function (err, data) {
+          if (err) {
+              res.writeHead(404, {'Content-Type': 'text/plain'});
+              res.write('not found!');
+              return res.end();
+          }
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+          res.end();
+      });
+}).listen(8000);
